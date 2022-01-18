e2c6afb945f9e682497bb0e0858555990ca55a34
Mihail Todorov <mihail@coherent-labs.com>
2022-01-18 15:37:30 +0200

    Add player command

--- src/commands/player.js
+++ src/commands/player.js
@@ -1,7 +1,47 @@
 const { Command, flags } = require('@oclif/command');
+const chalk = require('chalk');
+const { execFile } = require('child_process');
+const path = require('path');
+const { cwd } = require('process');
+const { CONFIG_NAME, CONFIG_EXTENSION } = require('../shared/config');
+const prompt = require('../shared/prompt');
+
+const { readConfig, checkValidConfig, getPlayerAndCohtml, saveConfigToFolder } = require('../shared/utils');
 
 class PlayerCommand extends Command {
-    async run() {}
+    static args = [
+        {
+            name: 'filePath',
+            required: false,
+            description: 'the file or url you want to open in the Player'
+        }
+    ];
+
+    async run() {
+        const { flags, args } = this.parse(PlayerCommand);
+        if (!args.filePath && !flags.update) {
+            console.log(
+                chalk.yellowBright('If you want to open a file you need to pass the path to it otherwise you can update the package in the config using the --update flag')
+            );
+            return;
+        }
+        const configPath = flags.config || `./${CONFIG_NAME}${CONFIG_EXTENSION}`;
+        const configValid = await checkValidConfig(configPath); //Checking if it's a valid config
+
+        if (configValid) {
+            if (flags.update) {
+                configValid.packagePath = await prompt.askPath();
+                saveConfigToFolder('', configValid);
+                if (!args.filePath) return;
+            }
+
+            const pathToFile = path.join(cwd(), args.filePath).replaceAll(`\\`, '/');
+            const { player: playerPath } = await getPlayerAndCohtml(configValid.packagePath);
+            execFile(playerPath, ['--player', `--url=${pathToFile}`, '--root'], (err) => {
+                if (err) throw new Error(err);
+            });
+        }
+    }
 }
 
 PlayerCommand.description = `Describe the command here
@@ -9,6 +49,13 @@ PlayerCommand.description = `Describe the command here
 Extra documentation goes here
 `;
 
-PlayerCommand.flags = {};
+PlayerCommand.flags = {
+    config: flags.string({
+        description: 'set path to config'
+    }),
+    update: flags.boolean({
+        description: 'update the package path in the config'
+    })
+};
 
 module.exports = PlayerCommand;
--- src/shared/Creator.js
+++ src/shared/Creator.js
@@ -1,11 +1,18 @@
 const { readConfig } = require('./utils');
 const Listr = require('listr');
 const TaskList = require('./TaskList');
+/**
+ * Creates a boilerplate from the config
+ */
 class Creator {
     constructor(name) {
         this.config = JSON.parse(readConfig(name));
     }
 
+    /**
+     * Chooses which boilerplate to create based on the type of project
+     * @returns {Promise}
+     */
     create() {
         const taskList = new TaskList(this.config);
         switch (this.config.type) {
