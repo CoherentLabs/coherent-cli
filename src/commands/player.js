50e5461aacacf4616c4f22e8f87db82b6e15c8a5
Mihail Todorov <mihail@coherent-labs.com>
2022-06-15 16:04:23 +0300

    Add support for different folder structure and support for Mac

--- src/commands/player.js
+++ src/commands/player.js
@@ -1,13 +1,14 @@
 const { Command, flags } = require('@oclif/command');
 const chalk = require('chalk');
-const { execFile } = require('child_process');
+const { execFile, execSync } = require('child_process');
 const path = require('path');
 const fs = require('fs-extra');
-const { cwd } = require('process');
+const { cwd, platform } = require('process');
 const { CONFIG_NAME, CONFIG_EXTENSION } = require('../shared/config');
 const prompt = require('../shared/prompt');
 
 const { isValidConfig, getPlayerAndCohtml, saveConfigToFolder, isNodeVersionSupported } = require('../shared/utils');
+const { glob } = require('glob');
 
 class PlayerCommand extends Command {
     static args = [
@@ -20,12 +21,15 @@ class PlayerCommand extends Command {
 
     async run() {
         const { flags, args } = this.parse(PlayerCommand);
+        const isMac = platform === 'darwin';
 
         if (!isNodeVersionSupported()) return;
 
         if (!args.filePath && !flags.update && !flags.config) {
             console.log(
-                chalk.yellowBright('If you want to open a file you need to pass the path to it otherwise you can update the package in the config using the --update flag')
+                chalk.yellowBright(
+                    'If you want to open a file you need to pass the path to it otherwise you can update the package in the config using the --update flag'
+                )
             );
             return;
         }
@@ -63,11 +67,7 @@ class PlayerCommand extends Command {
 
         if (file.type === 'file') this.validateFile(file.path);
 
-        const { player: playerPath } = await getPlayerAndCohtml(configValid.packagePath);
-
-        execFile(playerPath, ['--player', `--url=${file.path}`, '--root'], (err) => {
-            if (err) throw new Error(err);
-        });
+        isMac ? this.handleMac(configValid.packagePath, file.path) : this.handleWin(configValid.packagePath, file.path);
     }
     validateFile(filePath) {
         if (!fs.existsSync(filePath)) this.error('You need to pass a valid file or URL (with the http:// protocol)');
@@ -75,6 +75,28 @@ class PlayerCommand extends Command {
         if (!fs.lstatSync(filePath).isFile()) this.error('You need to pass a file not directory');
 
         if (path.extname(filePath) !== '.html') this.error('You need to pass a html file to the Player');
+    }
+
+    handleWin(packagePath, fileToOpen) {
+        const { player: playerPath } = getPlayerAndCohtml(packagePath);
+
+        execFile(playerPath, ['--player', `--url=${fileToOpen}`, '--root'], (err) => {
+            if (err) throw new Error(err);
+        });
+    }
+
+    handleMac(packagePath, fileToOpen) {
+        const macPath = glob.sync(`${packagePath}/**/CohtmlPlayer.app`)[0];
+        try {
+            execSync(`chmod +x ${macPath}/Contents/MacOS/CohtmlPlayer`);
+        } catch (error) {
+            throw new Error(err);
+        }
+        try {
+            execSync(`open ${macPath} --args --player --url=${fileToOpen}`);
+        } catch (error) {
+            throw new Error(err);
+        }
     }
 }
 
